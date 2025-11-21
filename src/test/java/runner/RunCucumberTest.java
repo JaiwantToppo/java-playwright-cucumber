b00bd0227be2cc9cb0e9c86f5bfda2171ab6e805
JaiwantToppo <toppojaiwant@gmail.com>
2025-11-21 14:42:50 +0530

    updated parallel execution

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -68,7 +68,7 @@ public class RunCucumberTest extends AbstractTestNGCucumberTests {
     }
 
     @Override
-    @DataProvider(parallel = false)
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
